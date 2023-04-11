fe145bc18a6e3da75422c474b06e3f66c61fbc11
VinicioSantosOliveira <viniciodossantosoliveira@hotmail.com>
2023-04-11 12:20:31 -0300

    argumentos

---
+++ Java/programas/argumentos.java
@@ -0,0 +1,8 @@
+public class argumentos {
+    public static void main(String[] args) {
+        arg(2, 4); //argumentos
+    }
+    static void arg(int x, int y){ //parametros
+        System.out.println(x + y);
+    }
+}
--- Java/programas/funcao.java
+++ Java/programas/funcao.java
@@ -1,12 +1,12 @@
 public class funcao {
     public static void main(String[] args) {
-        soma(2, 2);
+        soma(2, 2); //argumentos
         System.out.println("--------------------");
         print();
         System.out.println("--------------------");
-        soma(10, 5);
+        soma(10, 5); //argumentos
         System.out.println("--------------------");
-        calc(2, 2);
+        calc(2, 2); //argumentos
     }
     static void soma(int x, int y){
         System.out.println(x + y);
@@ -14,7 +14,7 @@ public class funcao {
     static void print(){
         System.out.println("Hello");
     }
-    static void calc(int x, int y){
+    static void calc(int x, int y){ //parametros
         System.out.println(x + y);
         System.out.println(x - y);
         System.out.println(x * y);
