554dcdb407463dbd21a022e545fac4608f9f8ccd
VinicioSantosOliveira <viniciodossantosoliveira@hotmail.com>
2023-04-16 15:51:41 -0300

    arrumei o codigo de condicoes

--- Java/programas/condicoes.java
+++ Java/programas/condicoes.java
@@ -7,48 +7,48 @@ public class condicoes {
         System.out.println(scanner);
         String valor = scanner.nextLine();
 
-        if (valor.equals("soma")){
+        if (valor.equals("somar")){
             int x, y;
             System.out.println("digite o valor de x: ");
             x = scanner.nextInt();
             System.out.println("digite o valor de y: ");
             y = scanner.nextInt();
-            soma(x, y);
+            somar(x, y);
         }else  if (valor.equals("subtrair")){
             int x, y;
             System.out.println("digite o valor de x: ");
             x = scanner.nextInt();
             System.out.println("digite o valor de y: ");
             y = scanner.nextInt();
-            soma(x, y);
+            subtrair(x, y);
         }else  if (valor.equals("multiplicar")){
             int x, y;
             System.out.println("digite o valor de x: ");
             x = scanner.nextInt();
             System.out.println("digite o valor de y: ");
             y = scanner.nextInt();
-            soma(x, y);
+            multiplicar(x, y);
         }else if (valor.equals("dividir")){
             int x, y;
             System.out.println("digite o valor de x: ");
             x = scanner.nextInt();
             System.out.println("digite o valor de y: ");
             y = scanner.nextInt();
-            soma(x, y);
+            dividir(x, y);
         }else {
             System.out.println("Nenhuma instruçao foi passada!");
         }
     }
-    static void soma(int x, int y){
+    static void somar(int x, int y){
         System.out.println(x + y);
     }
     static void subtrair(int x, int y){
-        System.out.println(x + y);
+        System.out.println(x - y);
     }
-    static void multiplica(int x, int y){
-        System.out.println(x + y);
+    static void multiplicar(int x, int y){
+        System.out.println(x * y);
     }
     static void dividir(int x, int y){
-        System.out.println(x + y);
+        System.out.println(x / y);
     }
 }
